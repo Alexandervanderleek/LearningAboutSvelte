b8b1a16a5462f656861a57c6437cb4ec6c31dc28
Alexander van der Leek <alexandervanderleek@gmail.com>
2025-05-31 12:30:20 +0200

    Added basic svelte frontend

---
+++ frontend/src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button onclick={increment}>
+  count is {count}
+</button>
+
+{#if count > 2}
+  <p>Large</p>
+{/if}
